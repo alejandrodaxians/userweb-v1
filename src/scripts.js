0d340eb6edaeb45a10f8ca132cdb43b87db18727
Alejandro Diaz <alejandro.diaz@axians.es>
2022-04-07 08:08:35 +0200

    Add scripts.js for API methods

---
+++ src/scripts.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+
+  el: '#main',
+  data () {
+    return {
+      result: '',
+      responseAvailable: false,
+      apiKey: ''
+
+    }
+  },
+
+  methods: {
+
+    fetchAPIData () {
+    }
+  }
+
+})
