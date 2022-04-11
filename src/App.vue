23be7fdcbcb614033f5e78e42e4f73e019b74790
Alejandro Diaz <alejandro.diaz@axians.es>
2022-04-11 16:59:54 +0200

    Changed order of navbar to add dropdown menu

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <nav>
     <router-link class="header" id= "search" to="/">SEARCH</router-link>
+    <router-link class="header" id= "get" to="/getId">GET</router-link>
     <router-link class="header" id= "send" to="/create">CREATE</router-link>
     <router-link class="header" id= "delete" to="/delete">DELETE</router-link>
-    <router-link class="header" id= "get" to="/getId">GET</router-link>
     <router-link class="header" id="config" to="/configure">CONFIGURE</router-link>
   </nav>
   <router-view/>
@@ -13,6 +13,11 @@
 export default {
   name: 'App',
   components: {
+  },
+  methods: {
+    myFunction () {
+      document.getElementById('dropdown').classList.toggle('show')
+    }
   }
 }
 </script>
@@ -21,5 +26,4 @@ export default {
 @import '../src/assets/style/darkstyle.scss';
 
 @import url('https://fonts.googleapis.com/css2?family=Sen&display=swap');
-
 </style>
