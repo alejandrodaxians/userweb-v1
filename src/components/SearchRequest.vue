296a840f032870916a59f3388c11ef1774759648
Alejandro Diaz <alejandro.diaz@axians.es>
2022-04-18 16:23:44 +0200

    Changed checkbox positioning, added table structure

--- src/components/SearchRequest.vue
+++ src/components/SearchRequest.vue
@@ -2,44 +2,49 @@
   <h4 class="title">{{ msg }}</h4>
   <div class="search">
   <form form @submit.prevent="search">
-    <div class="column">
+    <div class="fieldset">
+      <br>
       <h4>Parameters:</h4>
-      <input type="checkbox" id="complete" value="complete" name="complete" v-model="searchData.complete">
-      <label for="complete">complete</label>
       <br>
       <br>
-      <label for="sede">saga_name</label>
+      <p>
+        <label for="complete">complete</label>
+        <input type="checkbox" id="complete" value="complete" name="complete" v-model="searchData.complete">
+      </p>
       <br>
-      <select name="sede" id="sede" required>
-        <option value="crear">crear_sede</option>
-        <option value="borrar">borrar_sede</option>
-      </select>
+      <p>
+        <label for="sede">saga_name</label>
+        <select name="sede" id="sede" required>
+          <option value="crear">crear_sede</option>
+          <option value="borrar">borrar_sede</option>
+        </select>
+      </p>
       <br>
+      <p>
+        <label for="status">status</label>
+        <select id="status" name="status" v-model="searchData.status" required>
+          <option value="" disabled selected hidden></option>
+          <option value="acknowledged">ACKNOWLEDGED</option>
+          <option value="progress">IN-PROGRESS</option>
+          <option value="pending">PENDING</option>
+          <option value="held">HELD</option>
+          <option value="cancelled">CANCELLED</option>
+          <option value="completed">COMPLETED</option>
+          <option value="failed">FAILED</option>
+          <option value="partial">PARTIAL</option>
+          <option value="rejected">REJECTED</option>
+        </select>
+      </p>
       <br>
-      <label for="status">status</label>
-      <br>
-      <select id="status" name="status" v-model="searchData.status" required>
-        <option value="" disabled selected hidden></option>
-        <option value="acknowledged">ACKNOWLEDGED</option>
-        <option value="progress">IN-PROGRESS</option>
-        <option value="pending">PENDING</option>
-        <option value="held">HELD</option>
-        <option value="cancelled">CANCELLED</option>
-        <option value="completed">COMPLETED</option>
-        <option value="failed">FAILED</option>
-        <option value="partial">PARTIAL</option>
-        <option value="rejected">REJECTED</option>
-      </select>
-      <br>
-      <br>
+      <p>
       <label for="from">from_date</label>
-      <br>
       <input id="from" type="date" v-model="searchData.from" required/>
+      </p>
       <br>
+      <p>
       <label for="to">to_date</label>
-      <br>
       <input id="to" type="date" v-model="searchData.to" required/>
-      <br>
+      </p>
     </div>
     <div class="column6">
       <button class="reset" type="reset">RESET</button>
